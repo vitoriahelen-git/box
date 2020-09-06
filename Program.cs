c42539e525635a66bd2d23e002ae2a0af8ec29e2
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-05 22:44:29 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("|  Vitoria  |");
+            Console.WriteLine("+-----------+");
+        }
+    }
+}
